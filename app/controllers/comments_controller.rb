d23c89457151e062b1338e68a7e44d75bbed1ddc
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-03-04 20:06:06 +0000

    Ajaxified comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,6 +33,10 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_back fallback_location: root_path, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
+        format.js do
+          render template: "comments/create"
+        end
+        # can just write format.js if view folder matches controller name, template name matches action name, and request format matches file extension
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
